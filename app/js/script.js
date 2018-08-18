321d8bdeeb7169b851402aa506a5dfde119aa3a5
txucishvili <toc.xucishvili@gmail.com>
2018-08-18 04:44:41 +0400

    inits2

--- app/js/script.js
+++ app/js/script.js
@@ -20,12 +20,15 @@
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
+      console.log('hhh');
       td = tr[i].getElementsByTagName("td")[0];
       if (td) {
         if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
           tr[i].style.display = "";
+          tr[i].classList.add('-disabled');
         } else {
           tr[i].style.display = "none";
+          tr[i].classList.remove('-disabled');
         }
       }
     }
@@ -36,6 +39,7 @@
     var value = $(this).val().toLowerCase();
 
     $("#myTable tbody tr").filter(function() {
+      console.log($(this).classList);
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
